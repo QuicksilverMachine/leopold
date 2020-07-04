1cf461658f4382999a82446f813cb7396ce5e664
Dejan Knezevic <dejan.knezevic@sbgenomics.com>
2020-07-04 09:19:46 +0200

    Use json as handler response

--- src/api/tasks/handlers.rs
+++ src/api/tasks/handlers.rs
@@ -1,10 +1,12 @@
 use actix_web::{Responder, HttpResponse, web};
 
+use serde_json::json;
+
 use crate::tasks;
 use super::schema;
 
 
 pub async fn execute_task(request: web::Json<schema::TaskExecuteRequest>) -> impl Responder {
     tasks::execute_task(&request.app, &request.task_id).await;
-    HttpResponse::Ok().body("OK")
+    return HttpResponse::Ok().json(json!({"message": "OK"}))
 }
